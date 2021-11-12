cca4674300a0ee658f1e3a6aa5741408086f4df1
daxtonparke <daxtonparke@gmail.com>
2021-11-12 11:03:35 -0700

    sldc.js second time

--- js-basics-1/sldc.js
+++ js-basics-1/sldc.js
@@ -6,14 +6,20 @@
 */
 
 // Analysis of Requirements
+// Planning
 // Design
+// Testing & Integration
 // Implementation
 // Maintain
-// Planning
-// Testing & Integration
 
 
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
 */
+let Analysis = 'analyse what you code needs to do'
+let Planning = 'plan how your code will fill ints intended role'
+let Design = 'design how your code will achieve desired effect'
+let Testing = 'test that your code actually does as intended and integrate into application'
+let Implementation = 'implement youre code so it is functional on user side'
+let Maintain = 'correct bugs and add new features to keep code functional'
